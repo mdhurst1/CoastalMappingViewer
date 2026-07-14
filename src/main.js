10b01e5d1cb56ffdd1f8aeb8a7a219b43188e0ad
mdhurst1 <martin.d.hurst@gmail.com>
2026-07-14 18:37:38 +0100

    initial messing around to get a map working

--- src/main.js
+++ src/main.js
@@ -2,37 +2,55 @@ import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./style.css";
 
+console.log("main.js loaded");
+
 const map = new maplibregl.Map({
-    container: "map",
+  container: "map",
 
-    style: {
-        version: 8,
+  style: {
+    version: 8,
 
-        sources: {
-            basemap: {
-                type: "raster",
-                tiles: [
-                    "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-                ],
-                tileSize: 256,
-                attribution: "© OpenStreetMap contributors"
-            }
-        },
-
-        layers: [
-            {
-                id: "basemap",
-                type: "raster",
-                source: "basemap"
-            }
-        ]
+    sources: {
+      osm: {
+        type: "raster",
+        tiles: [
+          "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        ],
+        tileSize: 256,
+        attribution: "© OpenStreetMap contributors"
+      }
     },
 
-    center: [-2.45, 56.73],
-    zoom: 12
+    layers: [
+      {
+        id: "osm",
+        type: "raster",
+        source: "osm"
+      }
+    ]
+  },
+
+  center: [-2.45, 56.73],
+  zoom: 12
 });
 
 map.addControl(
-    new maplibregl.NavigationControl(),
-    "top-right"
+  new maplibregl.NavigationControl(),
+  "top-right"
 );
+
+map.addControl(
+  new maplibregl.ScaleControl({
+    maxWidth: 150,
+    unit: "metric"
+  }),
+  "bottom-left"
+);
+
+map.on("load", () => {
+  console.log("Map loaded successfully");
+});
+
+map.on("error", (event) => {
+  console.error("MapLibre error:", event.error);
+});
